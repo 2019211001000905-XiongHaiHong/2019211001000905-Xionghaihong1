c75d671031a4f55530b6628e6740508828b44675
17939 <1793992923@qq.com>
2021-04-26 19:16:43 +0800

    HW-week 8 Commit

---
+++ src/main/java/com/controller/UpdateUserServlet.java
@@ -0,0 +1,62 @@
+package com.controller;
+
+
+import com.dao.UserDao;
+import com.model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.SQLException;
+
+
+
+@WebServlet(name = "UpdateUserServlet", value = "/updateUser")
+public class UpdateUserServlet extends HttpServlet {
+    Connection con=null;
+
+    @Override
+    public void init() throws ServletException {
+        con = (Connection) getServletContext().getAttribute("con");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("WEB-INF/views/updateUser.jsp").forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        int id = Integer.parseInt(request.getParameter("id"));
+        String username=request.getParameter("username");
+        String password=request.getParameter("password");
+        String Email=request.getParameter("email");
+        String gender=request.getParameter("gender");
+        Date birthDate = Date.valueOf(request.getParameter("birthDate"));
+
+        User us=new User(id,username,password,Email,gender,birthDate);
+        us.setId(id);
+
+        UserDao userDao=new UserDao();
+        try {
+            int user =userDao.updateUser(con,us);
+            if (user !=0) {
+
+                request.getRequestDispatcher("/login").forward(request, response);
+            } else {
+                System.out.println("你的修改不符合格式");
+            }
+        }
+        catch(SQLException throwables){
+            throwables.printStackTrace();
+        }
+
+    }
+
+}
